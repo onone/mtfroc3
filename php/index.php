2a74fca018f909ccf3e362a2ead36db8bb8bad69
onenone <l122124x@gmail.com>
2014-01-16 09:26:08 -0500

    db test2

--- php/index.php
+++ php/index.php
@@ -7,35 +7,21 @@ require 'vendor/autoload.php';
 use RedBean_Facade as R;
 try {
     //R::setup("mysql:host={$config['host']};dbname={$config['dbname']};port={$config['port']}",$config['username'], $config['password']);
-    //R::setup("mysql:host=" . $_SERVER['SERVER_ADDR'] . ";dbname=php;port=3306",'langeli','');
-    R::setup("mysql:host=" . $OPENSHIFT_MYSQL_DB_HOST . ";dbname=php;port=$OPENSHIFT_MYSQL_DB_PORT",'adminUyLCLuc','JWmWpswfQHYg');
+    if(strpos($_SERVER['SCRIPT_FILENAME'],'stickshift') !== FLASE){
+        R::setup("mysql:host=" . $_SERVER['SERVER_ADDR'] . ";dbname=php;port=3306",'langeli','');
+    }else{
+        R::setup("mysql:host=" . $OPENSHIFT_MYSQL_DB_HOST . ";dbname=php;port=$OPENSHIFT_MYSQL_DB_PORT",'adminUyLCLuc','JWmWpswfQHYg'); 
+    }
+    
     R::freeze(true);
 } catch (Exception $e ) {
     echo $e->getMessage();
 }
 
-
-
-
+/*
 echo '<pre>';
-print_r(R::find('client'));
-echo '</pre>';
-
-$connessione = mysql_connect("127.9.24.129:3306", 'langeli')
-        or die("Connessione non riuscita: " . mysql_error());
-    print ("Connesso con successo");
-    mysql_close($connessione);
-
-
-
-var_dump(class_exists('\Slim\Slim'));
-var_dump(class_exists('R'));
-var_dump(class_exists('\RedBean\Facade'));
-var_dump(class_exists('\RedBean\RedBean_Facade'));
-var_dump(class_exists('RedBean_Facade'));
-
-
-echo 'hello';
+print_r($_SERVER);
+echo '</pre>';*/
 
 // initialize app
 $app = new \Slim\Slim();
