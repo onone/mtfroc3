0e5b2ca41d44beebbf31a814a0e57e52d4474d93
onenone <l122124x@gmail.com>
2014-06-22 19:30:27 +0000

    quai finito

--- php/app/routes/login_logout.php
+++ php/app/routes/login_logout.php
@@ -11,51 +11,40 @@ $app->get('/logout', $authAdmin('admin'), function () use ($app) {
 
 })->name('logout');
 
-$app->map('/login', function () use ($app) {
-    // Test for Post & make a cheap security check, to get avoid from bots
+$app->get('/login', function () use ($app) {
     
-    //$isPost = $app->request()->isPost();
-    $request = $app->request;
-    $method = $request->getMethod();
-    
-    $app->setEncryptedCookie('time',time());
-    $app->setEncryptedCookie('method',$method);
-    $app->setEncryptedCookie('request',json_encode($request));
-    //if($isPost === TRUE)
-    if($method == 'POST'){
-        
-        $app->setEncryptedCookie('line',__LINE__);
-        // Don't forget to set the correct attributes in your form (name="user" + name="password")
-        
-        $post = $request->post();
-        
-        if(isset($post['user']) && isset($post['password']))
-        {
-            
-            $app->setEncryptedCookie('line',__LINE__);
-            $app->setEncryptedCookie('user',$post['user']);
-            $app->setEncryptedCookie('pwd',$post['password']);
-            
-            $loginUrl = $app->getEncryptedCookie('loginUrl',false);
-            if($loginUrl){
-                if($loginUrl == 'https://' . $_SERVER['SERVER_NAME'] . $app->urlFor('login')){
-                    $loginUrl = 'https://' . $_SERVER['SERVER_NAME'] . $app->urlFor('entityListUI',array('entityName' => 'client'));
-                }
-                $app->redirect($loginUrl);
-            }else{
-                $app->redirect('https://' . $_SERVER['SERVER_NAME'] . $app->urlFor('entityListUI',array('entityName' => 'client')));
-            }
-            
-            
-        } 
-        else
-        {
-            $app->redirect('https://' . $_SERVER['SERVER_NAME'] . $app->urlFor('login'));
-        }
-    }
+    $app->setEncryptedCookie('login get',date("H:i:s u"));
     // render login
     $app->render('login.html');
+})->name('login');
 
-})->via('GET','POST')->name('login');
+$app->post('/login', function () use ($app) {
+    $request = $app->request;
+    
+    $app->setEncryptedCookie('login post',date("H:i:s u"));
+    $post = $request->post();
+        
+    if(isset($post['user']) && isset($post['password']))
+    {
+        $app->setEncryptedCookie('user',$post['user']);
+        $app->setEncryptedCookie('pwd',$post['password']);
+        
+        $loginUrl = $app->getEncryptedCookie('loginUrl',false);
+        if($loginUrl){
+            if($loginUrl == 'https://' . $_SERVER['SERVER_NAME'] . $app->urlFor('login')){
+                $loginUrl = 'https://' . $_SERVER['SERVER_NAME'] . $app->urlFor('entityListUI',array('entityName' => 'client'));
+            }
+            $app->redirect($loginUrl);
+        }else{
+            $app->redirect('https://' . $_SERVER['SERVER_NAME'] . $app->urlFor('entityListUI',array('entityName' => 'client')));
+        }
+        
+    } 
+    else
+    {
+        $app->redirect('https://' . $_SERVER['SERVER_NAME'] . $app->urlFor('login'));
+    }
+
+});
 
 ?>
