0b18a242df200b058882cd2d61647af434aa9627
onenone <l122124x@gmail.com>
2015-11-21 16:49:39 +0000

    modifica anno 2015 con 2020

--- php/index.php
+++ php/index.php
@@ -5,30 +5,16 @@ session_start();
 define('START_TIME',microtime(TRUE));
 $GLOBALS['timings'][] = array('start' => microtime(TRUE));
 
-echo '<br>'  . __LINE__;
 // AUTOLOAD DI COMPOSER
-echo '<br>' . __DIR__ . '/vendor/autoload.php';
 if(file_exists(__DIR__ . '/vendor/autoload.php')){
-    
-    
-echo '<br>File esiste';
-    echo file_get_contents(__DIR__ . '/vendor/autoload.php');
     try {
         require __DIR__ . '/vendor/autoload.php';
     } catch (Exception $e ) {
         echo $e->getMessage();
     }
-}else{
-    
-echo '<br>File non esiste';
 }
-echo '<br>'  . __LINE__;
-
 
 use RedBean_Facade as R;
-
-echo '<br>'  . __LINE__;
-
 
 // MODALITA DI ESECUZIONE
 $app_mode = 'production';
@@ -37,20 +23,16 @@ if(strpos($_SERVER['HTTP_HOST'],'mftr3-c9-langeli.c9.io') !== FALSE){ // VARIABI
     $app_mode = 'development';
 }
 
-echo '<br>'  . __LINE__;
 // INIZIALIZZO  APP
 $app = new \Slim\Slim(array(
     'mode' => $app_mode
 ));
 
-echo '<br>'  . __LINE__;
 $app->response->headers->set('Content-Type', 'text/html; charset=utf-8');
 
-echo '<br>'  . __LINE__;
 // CONFIGURAZIONE
 include __DIR__ . '/app/config/config.php';
 
-echo '<br>'  . __LINE__;
 // EXCEPTIONS
 require APP_PATH . '/lib/exceptions.php';
 
@@ -58,7 +40,6 @@ require APP_PATH . '/lib/exceptions.php';
 require APP_PATH . '/lib/functions/authAdmin.php';
 require APP_PATH . '/lib/functions/entityRepresentation.php';
 
-echo '<br>'  . __LINE__;
 // ROTTE
 require APP_PATH . '/routes/login_logout.php';
 
@@ -69,7 +50,6 @@ require APP_PATH . '/routes/resources/performancePaymentList.php';
 require APP_PATH . '/routes/resources/DefaultEntity.php';
 require APP_PATH . '/routes/resources/dump.php';
 
-echo '<br>'  . __LINE__;
 // STATS
 require APP_PATH . '/routes/stats/revenuesStat.php';
 
@@ -79,9 +59,6 @@ require APP_PATH . '/routes/resources/XEditable.php';
 require APP_PATH . '/routes/development/test.php'; // ROTTE DI TEST
 require APP_PATH . '/routes/development/populate.php'; // POPOLAZIONE DB
 
-
-echo '<br>'  . __LINE__;
-die();
 
 // ESEGUO APP
 $app->run();
